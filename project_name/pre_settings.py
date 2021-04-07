a0b0c88ef20ec0e39c0b3711fc3465edcf085965
Ahmed Nour Eldeen <ahmednoreldeen@gmail.com>
2021-04-07 17:47:12 +0200

    set default uploader to geonode.importer
    (cherry picked from commit 93aa01188c4ec625fe15989f74b3f097add42d4d)

--- project_name/pre_settings.py
+++ project_name/pre_settings.py
@@ -5,3 +5,5 @@ from distutils.sysconfig import get_python_lib
 
 if os.name == 'nt':
     os.environ['Path'] = (r'%s\osgeo;' % (get_python_lib())) + os.environ['Path']
+
+os.environ.setdefault('DEFAULT_BACKEND_UPLOADER', 'geonode.importer')
