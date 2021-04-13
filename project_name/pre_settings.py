ee2e04481617f25b469643a4bed4cbab252aa81d
Ahmed Nour Eldeen <ahmednoreldeen@gmail.com>
2021-04-13 15:06:00 +0200

    set APPS_DIR default value relative to project directory
    (cherry picked from commit a7164607adc16d825b7681a4736618358161f8ef)

--- project_name/pre_settings.py
+++ project_name/pre_settings.py
@@ -1,9 +1,11 @@
-# This settings file to setup setting must be imported before importing geonode settings to affect
-
 import os
 from distutils.sysconfig import get_python_lib
 
 if os.name == 'nt':
     os.environ['Path'] = (r'%s\osgeo;' % (get_python_lib())) + os.environ['Path']
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+os.environ.setdefault('APPS_DIR', os.path.abspath(os.path.join(BASE_DIR, "apps")))
 
 os.environ.setdefault('DEFAULT_BACKEND_UPLOADER', 'geonode.importer')
